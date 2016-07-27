78fab5fe1564a7718525a99213711ec8d69eebb2
skeleton <marie.bochu@akeneo.com>
2016-07-27 17:02:28 +0200

    TIP-451: add a working directory for media

--- src/Pim/Component/Connector/Processor/Normalization/VariantGroupProcessor.php
+++ src/Pim/Component/Connector/Processor/Normalization/VariantGroupProcessor.php
@@ -70,7 +70,8 @@ class VariantGroupProcessor extends AbstractConfigurableStepElement implements
         ]);
 
         $parameters = $this->stepExecution->getJobParameters();
-        $this->importMedia($variantGroup, $parameters);
+        $directory = $this->getWorkingDirectory($parameters->get('filePath'));
+        $this->importMedia($variantGroup, $directory);
 
         $this->objectDetacher->detach($variantGroup);
 
@@ -89,15 +90,14 @@ class VariantGroupProcessor extends AbstractConfigurableStepElement implements
      * Import media in local filesystem
      *
      * @param GroupInterface $variantGroup
-     * @param JobParameters  $parameters
+     * @param string         $directory
      */
-    protected function importMedia(GroupInterface $variantGroup, JobParameters $parameters)
+    protected function importMedia(GroupInterface $variantGroup, $directory)
     {
         if (null === $productTemplate = $variantGroup->getProductTemplate()) {
             return;
         }
 
-        $directory = dirname($parameters->get('filePath'));
         $identifier = $variantGroup->getCode();
         $this->variantGroupUpdater->update($variantGroup, ['values' => $productTemplate->getValuesData()]);
 
@@ -106,5 +106,25 @@ class VariantGroupProcessor extends AbstractConfigurableStepElement implements
         foreach ($this->mediaExporter->getErrors() as $error) {
             $this->stepExecution->addWarning($error['message'], [], $error['media']);
         }
+    }
+
+    /**
+     * Build path of the working directory to import media in a specific directory.
+     * Will be extracted with TIP-539
+     *
+     * @param string $filePath
+     *
+     * @return string
+     */
+    protected function getWorkingDirectory($filePath)
+    {
+        $jobExecution = $this->stepExecution->getJobExecution();
+
+        return dirname($filePath)
+            . DIRECTORY_SEPARATOR
+            . $jobExecution->getJobInstance()->getCode()
+            . DIRECTORY_SEPARATOR
+            . $jobExecution->getId()
+            . DIRECTORY_SEPARATOR;
     }
 }
